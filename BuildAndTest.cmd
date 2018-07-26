1ed57314d0375a031032c1a5c93aa623f702c219
Everett Maus <evmaus@microsoft.com>
2018-07-26 11:36:22 -0700

    Stashing some build experiments.

--- BuildAndTest.cmd
+++ BuildAndTest.cmd
@@ -19,44 +19,14 @@ SET NuGetConfigFile=%~dp0src\NuGet.config
 set NuGetPackageDir=%~dp0src\packages
 set NuGetOutputDirectory=%~dp0bld\bin\nuget\
 
-call SetCurrentVersion.cmd
+call WriteVersionConstants.cmd
 
-set VERSION_CONSTANTS=%~dp0src\BinaryParsers\VersionConstants.cs
+call Build.cmd || goto :ExitFailed
 
-@REM Rewrite VersionConstants.cs
-echo // Copyright (c) Microsoft. All rights reserved. Licensed under the MIT         >  %VERSION_CONSTANTS%
-echo // license. See LICENSE file in the project root for full license information.  >> %VERSION_CONSTANTS%
-echo namespace Microsoft.CodeAnalysis.IL                                             >> %VERSION_CONSTANTS%
-echo {                                                                               >> %VERSION_CONSTANTS%
-echo     public static class VersionConstants                                        >> %VERSION_CONSTANTS%
-echo     {                                                                           >> %VERSION_CONSTANTS%
-echo         public const string Prerelease = "%PRERELEASE%";                        >> %VERSION_CONSTANTS%
-echo         public const string AssemblyVersion = "%MAJOR%.%MINOR%.%PATCH%" + ".0"; >> %VERSION_CONSTANTS%
-echo         public const string FileVersion = "%MAJOR%.%MINOR%.%PATCH%" + ".0";     >> %VERSION_CONSTANTS%
-echo         public const string Version = AssemblyVersion + Prerelease;             >> %VERSION_CONSTANTS%
-echo     }                                                                           >> %VERSION_CONSTANTS%
-echo  }                                                                              >> %VERSION_CONSTANTS%
-
-%~dp0.nuget\NuGet.exe restore %~dp0src\BinSkim.sln -ConfigFile "%NuGetConfigFile%" -OutputDirectory "%NuGetPackageDir%"
-
-:: Build the solution 
-dotnet clean /verbosity:minimal %~dp0src\BinSkim.sln /p:Configuration=%Configuration% || goto :ExitFailed
-dotnet build /verbosity:minimal %~dp0src\BinSkim.sln /p:Configuration=%Configuration% /filelogger /fileloggerparameters:Verbosity=detailed || goto :ExitFailed
-
-::Run unit tests
-echo Run all multitargeting xunit tests
-call :RunTestProject BinaryParsers Unit || goto :ExitFailed
-call :RunTestProject BinSkim.Rules Unit || goto :ExitFailed
-call :RunTestProject BinSkim.Driver Functional || goto :ExitFailed
-call :RunTestProject BinSkim.Rules Functional  || goto :ExitFailed
+:: call Test.cmd || goto :ExitFailed
 
 ::Create the BinSkim platform specific publish packages
-echo Creating Platform Specific BinSkim 'Publish' Packages
-call :CreatePublishPackage net461 win-x86 || goto :ExitFailed
-call :CreatePublishPackage net461 win-x64 || goto :ExitFailed
-call :CreatePublishPackage netcoreapp2.0 win-x86 || goto :ExitFailed
-call :CreatePublishPackage netcoreapp2.0 win-x64 || goto :ExitFailed
-call :CreatePublishPackage netcoreapp2.0 linux-x64 || goto :ExitFailed
+call CreatePublishPackages.cmd || goto :ExitFailed
 
 ::Build NuGet package
 echo BuildPackages.cmd
@@ -67,19 +37,6 @@ echo CreateLayoutDirectory.cmd %~dp0bld\bin %Configuration% %Platform%
 call CreateLayoutDirectory.cmd %~dp0bld\bin %Configuration% %Platform%
 
 goto :Exit
-
-:RunTestProject
-set TestProject=%1
-set TestType=%2
-pushd %~dp0src\Test.%TestType%Tests.%TestProject% && dotnet test --no-build -c %Configuration% && popd
-if "%ERRORLEVEL%" NEQ "0" (echo %TestProject% %TestType% tests execution FAILED.)
-Exit /B %ERRORLEVEL%
-
-:CreatePublishPackage
-set Framework=%~1
-set RuntimeArg=%~2
-dotnet publish %~dp0src\BinSkim.Driver\BinSkim.Driver.csproj --no-restore -c %Configuration% -f %Framework% --runtime %RuntimeArg%
-Exit /B %ERRORLEVEL%
 
 :ExitFailed
 @echo Build and test did not complete successfully.
